31664d4488249cf3d7bd193186af8b1f756ea190
Vít Čech <228680@vutbr.cz>
2020-12-06 15:24:42 +0100

    World_UP
    Aktualizováno na testování kamery a senzoru

--- controllers/twister_controller/twister_controller.m
+++ controllers/twister_controller/twister_controller.m
@@ -16,15 +16,10 @@ TIME_STEP = 64;
 %  camera = wb_robot_get_device('camera');
 %  wb_camera_enable(camera, TIME_STEP);
 %  motor = wb_robot_get_device('motor');
-<<<<<<< Updated upstream
-
-
-=======
 rotational_motor = wb_robot_get_device('twister');
 wb_motor_set_position(rotational_motor, inf);
 %wb_position_sensor_sampling_period = 100;
 %wb_position_sensor_enable(twister_sensor);
->>>>>>> Stashed changes
 
 %wb_position_sensor_enable(PositionSensor);
   % Process here sensor data, im
@@ -33,23 +28,19 @@ wb_motor_set_position(rotational_motor, inf);
 % perform simulation steps of TIME_STEP milliseconds
 % and leave the loop when Webots signals the termination
 %
+while wb_robot_step(TIME_STEP) ~= -1
 
   % read the sensors, e.g.:
   %  rgb = wb_camera_get_image(camera);
 
   % Process here sensor data, images, etc.
-<<<<<<< Updated upstream
-   
-=======
     wb_motor_set_velocity(rotational_motor, 0.1);
     %value = wb_position_sensor_get_value(PositionSensor)
    % if mod(value, 3) == 0
       %wb_motor_set_velocity(rotational_motor, -0.1);
     %end 
     %value = wb_position_sensor_get_value(twister_sensor)
->>>>>>> Stashed changes
     
-   
   % send actuator commands, e.g.:
   %  wb_motor_set_postion(motor, 10.0);
 
