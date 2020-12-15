8f178fe4917b9e274f17d8b11e11790de6627798
Vít Čech <228680@vutbr.cz>
2020-12-15 08:09:12 +0100

    Update twister_controller.m

--- controllers/twister_controller/twister_controller.m
+++ controllers/twister_controller/twister_controller.m
@@ -7,7 +7,11 @@
 
 % uncomment the next two lines if you want to use
 % MATLAB's desktop to interact with the controller:
+<<<<<<< HEAD
 desktop;
+=======
+%desktop;
+>>>>>>> world
 %keyboard;
 
 TIME_STEP = 64;
@@ -15,12 +19,17 @@ TIME_STEP = 64;
 
 rotational_motor = wb_robot_get_device('twister');
   wb_motor_set_position(rotational_motor, inf);
+<<<<<<< HEAD
 twister_pos = wb_motor_get_position_sensor(rotational_motor);
+=======
+twister_pos = wb_motor_get_position_sensor(twister);
+>>>>>>> world
   wb_position_sensor_enable(twister_pos, TIME_STEP);
 
 
 while wb_robot_step(TIME_STEP) ~= -1
 
+<<<<<<< HEAD
 angle = wb_position_sensor_get_value(twister_pos)
  
  if angle < 5
@@ -32,6 +41,11 @@ angle = wb_position_sensor_get_value(twister_pos)
 
    
 
+=======
+ 
+wb_motor_set_velocity(rotational_motor, 0.5);
+value = wb_position_sensor_get_value(twister_pos);    
+>>>>>>> world
 
     
   drawnow;
